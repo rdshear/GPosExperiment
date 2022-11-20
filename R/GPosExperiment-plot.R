5220b129c366021c53a87096414798d7f831559f
Robert D Shear <rshear@gmail.com>
2022-11-20 16:33:31 -0500

    improve formatting, still WIP

--- R/GPosExperiment-plot.R
+++ R/GPosExperiment-plot.R
@@ -10,21 +10,31 @@
 {
   oldpar <- par(no.readonly = TRUE)
 
+  # TODO split rownranges on seqname
   for (chr in unique(as.character(runValue(seqnames(rowRanges(object))))))
   {
-    genes <- genelist[seqnames(rowRanges(object)) == chr]
-    grtrack <- Gviz::GeneRegionTrack(genes)
-    atrack <- Gviz::AnnotationTrack(genes)
-    gtrack <- Gviz::GenomeAxisTrack(add53 = TRUE)
-    tracks <- c(gtrack, grtrack, atrack)
+    tracks <- list()
+    genes <- rowRanges(object) # TODO WIP [seqnames(rowRanges(object)) == chr]
+    tracks <- append(tracks, Gviz::GenomeAxisTrack(add53 = TRUE))
+    tracks <- append(tracks, Gviz::GeneRegionTrack(genes,
+                                                   name = chr,
+                                                   shape = "arrow",
+                                                   showId = TRUE,
+                                                   symbol=genes$tx_name, 
+                                                   add53 = TRUE))
+
+    # TODO Add sample names to the DataTrack name: colNames(scores)
     z <- unlist(apply(scores(object), 2, function(u) {
       w <- unlist(as(u, "GRangesList"))
-      lapply(split(w, strand(w), drop = TRUE), function(v)
-        Gviz::DataTrack(v, type = "h", transformation = function(x) log2(x)))
-    }
-    ))
+      w <- split(w, strand(w), drop=TRUE)
+      mapply(function(v, id)
+        Gviz::DataTrack(v, type = "h", name=id,
+                        transformation = function(x) log2(x)),
+        v = w, id = names(w))
+    }))
+        
     
-    Gviz::plotTracks(c(tracks, z), showId = TRUE)
+    Gviz::plotTracks(c(tracks, z))
   }
   
   par(oldpar)
